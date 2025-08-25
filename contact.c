186c2836218783a495aae7656521e688641de7df
ggs <ggs@ford.com>
2025-08-25 14:40:06 +0530

    Implemented the functionality for CreateContact & ListContacts

--- contact.c
+++ contact.c
@@ -8,7 +8,17 @@
 void listContacts(AddressBook *addressBook, int sortCriteria) 
 {
     // Sort contacts based on the chosen criteria
-    
+
+    if (addressBook->contactCount == 0)
+    {
+        printf("AddressBook is empty!\n");
+        return;
+    }
+
+    for (int i = 0; i < addressBook->contactCount; i++)
+    {
+        printf("%d. Name : %s\t Phone : %s\t Email : %s\n", i+1, addressBook->contacts[i].name, addressBook->contacts[i].phone, addressBook->contacts[i].email);
+    }
 }
 
 void initialize(AddressBook *addressBook) {
@@ -28,6 +38,25 @@ void saveAndExit(AddressBook *addressBook) {
 void createContact(AddressBook *addressBook)
 {
 	/* Define the logic to create a Contacts */
+
+    if (addressBook->contactCount >= MAX_CONTACTS)
+    {
+        printf("AddressBook is Full!\n");
+        return;
+    }
+    
+
+    Contact new_contact;
+    printf("Enter the name of contact : ");
+    scanf(" %[^\n]", new_contact.name);
+    printf("Enter the contact number : ");
+    scanf(" %[^\n]", new_contact.phone);
+    printf("Enter the email ID : ");
+    scanf(" %[^\n]", new_contact.email);
+
+    addressBook->contacts[addressBook->contactCount] = new_contact;
+    addressBook->contactCount++;
+    printf("Contact successfully added!\n");
     
 }
 
